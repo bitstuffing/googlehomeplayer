1db0af2122a5658ae6c84ab089efbe39dd890a5e
bitstuffing <eduardo.higuero@hotmail.com>
2018-11-20 21:21:08 +0100

    testing functional example with dependencies

--- player/views.py
+++ player/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from utils.administrationUtils import AdministrationUtils
 
 def index(request):
+    target = "Cocina"
+    import pychromecast
+    chromecasts = pychromecast.get_chromecasts()
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == target)
     context = {}
+    context["device"] = str(cast.device)
+    print(str(context))
     return AdministrationUtils.render(request,'player/index.html',context)
