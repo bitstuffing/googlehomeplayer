1fca13db970606bd8cb5594487edac81701b718b
bitstuffing <eduardo.higuero@hotmail.com>
2018-11-20 19:47:04 +0100

    default is player app

--- googlehomeplayer/urls.py
+++ googlehomeplayer/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('player/', include('player.urls'))
+    path('player/', include('player.urls')),
+    path('', include('player.urls'))
 ]
