c0b32fdf2917d89c948c4167d0807afbc17c10e5
bitstuffing <eduardo.higuero@hotmail.com>
2018-11-22 00:06:03 +0100

    split in api params

--- player/urls.py
+++ player/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('devices/', views.get_devices, name='get_devices'),
+    url('^device/(\w+)/$', views.select_device, name='select_device'),
+    url('^play/(\w+)/$', views.play, name='play')
 ]
--- player/views.py
+++ player/views.py
@@ -2,13 +2,34 @@ from django.shortcuts import render
 
 from utils.administrationUtils import AdministrationUtils
 
+import pychromecast
+import json
+
 def index(request):
-    target = "Cocina"
-    import pychromecast
-    chromecasts = pychromecast.get_chromecasts()
-    cast = next(cc for cc in chromecasts if cc.device.friendly_name == target)
     context = {}
-    context["device"] = str(cast.device)
-    mc = cast.media_controller
-    mc.play_media("http://www.hochmuth.com/mp3/Bloch_Prayer.mp3","audio/mp3")
     return AdministrationUtils.render(request,'player/index.html',context)
+
+def select_device(request,target):
+    request.session["device"] = target
+    data = {}
+    data["target"] = target
+    return AdministrationUtils.jsonResponse(data)
+
+def get_devices(request):
+    chromecasts = pychromecast.get_chromecasts()
+    devices = []
+    for cc in chromecasts:
+        devices.append(cc.device.friendly_name)
+    elements = json.dumps(devices)
+    return AdministrationUtils.httpResponse(elements)
+
+def play(request,url):
+    chromecasts = pychromecast.get_chromecasts()
+    target = request.session["device"]
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == target)
+    mc = cast.media_controller
+    finalUrl = base64.decodestring(url)
+    mc.play_media(finalUrl,"audio/mp3")
+    data = {}
+    data["url"] = url
+    return AdministrationUtils.jsonResponse(data)
