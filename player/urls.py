3698a9a3d26a9bb83e333ee24e9cd2fe99bd2944
bitstuffing <eduardo.higuero@hotmail.com>
2018-11-22 21:06:09 +0100

    improves related to start and stop

--- player/urls.py
+++ player/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('devices/', views.get_devices, name='get_devices'),
     url('^device/(\w+)/$', views.select_device, name='select_device'),
-    url('^play/(\w+)/$', views.play, name='play')
+    url('^play/(\w+)/$', views.play, name='play'),
+    url('^stop/', views.stop, name='stop'),
+    url('^pause/', views.stop, name='pause')
 ]
--- player/views.py
+++ player/views.py
@@ -3,15 +3,25 @@ from django.shortcuts import render
 from utils.administrationUtils import AdministrationUtils
 
 import pychromecast
+from pychromecast import Chromecast, DeviceStatus, CAST_TYPES, CAST_TYPE_CHROMECAST
 import json
 import base64
+
+MESSAGE_TYPE = 'type'
+TYPE_PAUSE = "PAUSE"
 
 def index(request):
     context = {}
     return AdministrationUtils.render(request,'player/index.html',context)
 
 def select_device(request,target):
-    request.session["device"] = target
+    chromecasts = pychromecast.get_chromecasts()
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == target)
+    request.session["ip_address"] = cast.host
+    request.session["port"] = cast.port
+    request.session["friendly_name"] = target
+    request.session["model_name"] = cast.model_name
+    request.session["uuid"] = str(cast.uuid)
     data = {}
     data["target"] = target
     return AdministrationUtils.jsonResponse(data)
@@ -25,14 +35,47 @@ def get_devices(request):
     return AdministrationUtils.httpResponse(elements)
 
 def play(request,url):
-    chromecasts = pychromecast.get_chromecasts()
-    target = request.session["device"]
-    cast = next(cc for cc in chromecasts if cc.device.friendly_name == target)
+    cast = getCast(request)
     mc = cast.media_controller
-    print("url: "+str(url))
     finalUrl = base64.b64decode(url).decode("utf-8")
     mc.play_media(finalUrl,"audio/mp3")
-    mc.play()
     data = {}
-    data["url"] = str(finalUrl)
+    data["playing"] = str(finalUrl)
     return AdministrationUtils.httpResponse(json.dumps(data))
+
+def stop(request):
+    cast = getCast(request)
+    mc = cast.media_controller
+    mc.block_until_active()
+    mc.stop()
+    data = {}
+    data["stop"] = "true"
+    return AdministrationUtils.jsonResponse(data)
+
+def pause(request):
+    cast = getCast(request)
+    mc = cast.media_controller
+    mc.block_until_active(timeout=2)
+    status = "true"
+    if mc.player_state == MEDIA_PLAYER_STATE_PLAYING:
+        mc.pause()
+    else:
+        status = "false"
+        mc.play()
+    data = {}
+    data["pause"] = status
+    return AdministrationUtils.jsonResponse(data)
+
+def getCast(request):
+    friendly_name = request.session["friendly_name"]
+    model_name = request.session["model_name"]
+    uuid = request.session["uuid"]
+    ip_address = request.session["ip_address"]
+    port = request.session["port"]
+    cast_type = CAST_TYPES.get(model_name.lower(), CAST_TYPE_CHROMECAST)
+    device = DeviceStatus(
+        friendly_name=friendly_name, model_name=model_name,
+        manufacturer=None, uuid=uuid, cast_type=cast_type
+    )
+    cast = Chromecast(host=ip_address, port=port, device=device)
+    return cast
