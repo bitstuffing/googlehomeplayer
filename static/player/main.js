525dd09bc16dafd3cdad5535461dd8bea5b6de3a
bitstuffing <eduardo.higuero@hotmail.com>
2018-11-24 18:44:41 +0100

    union with layout

--- static/player/main.js
+++ static/player/main.js
@@ -1,7 +1,43 @@
+
 $(document).ready(function(){
 
+  $("#inputLink").click(function(){
+    var link = $(this).val();
+    var encodedLink = encodeURI(btoa(link));
+    $.get("/play/"+encodedLink+"/",function(r){console.log(r)});
+  });
+
+  $("#stopIcon").click(function(){
+    $.get("/stop/",function(e){console.log(e)});
+  });
+
+  $("#pauseIcon").click(function(){
+    $.get("/pause/",function(e){console.log(e)});
+  });
+
   $("#selectDevice").click(function(){
-    $("#status").text("clicked!!");
+    $("#status").text("Obtaining devices...");
+    $("#loadingSpin").show();
+    $("#inputSelectDevice").hide();
+    $("#inputSelectDevice").attr("disabled","");
+    $.getJSON("/devices/",function(response){
+      $("#status").text("Select one device");
+      $("#inputSelectDevice").removeAttr("disabled");
+      $("#loadingSpin").hide();
+      $("#inputSelectDevice").show();
+      $.each(response,function(i,value){
+        var option = "<option value='"+value+"'>"+value+"</option>";
+        $("#inputSelectDevice").append(option);
+      });
+      $("#inputSelectDevice").change(function(){
+          var value = $(this).val();
+          $("#selectedDevice").val(value);
+          //$('#modalDevice').modal().hide();
+          $("#modalDevice .close").click();
+          $.get("/device/"+value+"/",function(r){console.log(r)});
+          $("#status").text("Selected: "+$("#selectedDevice").val());
+      });
+    });
   });
 
   $("#status").text("Ready!");
