f470863231fc08b7843a6ba5c347666ce5006ebf
bitstuffing <eduardo.higuero@hotmail.com>
2018-12-16 09:33:19 +0100

    fix bug related to space in selected device api

--- player/urls.py
+++ player/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('devices/', views.get_devices, name='get_devices'),
-    url('^device/(\w+)/$', views.select_device, name='select_device'),
+    url('^device/(.*)/$', views.select_device, name='select_device'),
     url('^play/', views.play, name='play'),
     url('^stop/', views.stop, name='stop'),
     url('^pause/', views.pause, name='pause'),
